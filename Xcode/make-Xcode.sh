041a2328de6699452098efc8f6d30babd505a9c4
Joe Levin <josephlevin@spotify.com>
2023-03-06 12:41:55 -0500

    Add sidetracked theme

--- Xcode/make-Xcode.sh
+++ Xcode/make-Xcode.sh
@@ -6,26 +6,26 @@
 
 ########## Variables
 
-dir=~/dotfiles/Xcode                    # dotfiles directory
-olddir=~/userdata_old             # old dotfiles backup directory
-dirs="CodeSnippets FontAndColorThemes"    # list of files/folders to symlink in homedir
+# dir=~/dotfiles/Xcode                    # dotfiles directory
+# olddir=~/userdata_old             # old dotfiles backup directory
+# dirs="FontAndColorThemes"    # list of files/folders to symlink in homedir
 
-##########
+# ##########
 
-# create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing UserData in ~"
-mkdir -p $olddir
-echo "...done"
+# # create dotfiles_old in homedir
+# echo "Creating $olddir for backup of any existing UserData in ~"
+# mkdir -p $olddir
+# echo "...done"
 
-# change to the dotfiles directory
-echo "Changing to the $dir directory"
-cd $dir
-echo "...done"
+# # change to the dotfiles directory
+# echo "Changing to the $dir directory"
+# cd $dir
+# echo "...done"
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
-for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
-done
+# # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+# for file in $files; do
+#     echo "Moving any existing dotfiles from ~ to $olddir"
+#     mv ~/.$file ~/dotfiles_old/
+#     echo "Creating symlink to $file in home directory."
+#     ln -s $dir/$file ~/.$file
+# done
